22ca5c546365f765a950b37ed9bc15de29649006
Dimitris Spathoulas <147658216+DimSpathoulas@users.noreply.github.com>
2024-07-25 15:20:40 +0300

    Update train_g2_final.py

--- train_g2_final.py
+++ train_g2_final.py
@@ -828,6 +828,9 @@ def track_nuscenes(data_split='train', match_threshold=11, save_root='/.results/
 
                         if D.shape[0] == 0:
                             continue
+                        
+                        optimizer.zero_grad()
+                        
                         D = D.to(device)
                         K = torch.tensor(K).to(device)
 
@@ -842,7 +845,6 @@ def track_nuscenes(data_split='train', match_threshold=11, save_root='/.results/
                         #         else:
                         #             print(f"No gradients for parameter '{name}'. Parameter was not updated.")
 
-                        optimizer.zero_grad()
                         optimizer.step()
 
                 cycle_time = time.time() - start_time
